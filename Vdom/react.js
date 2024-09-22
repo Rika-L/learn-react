15ecb37a3dd3ae55f0d85e0f77f4f37124b9e9e9
Rika-L <2209393334@qq.com>
2024-09-22 18:25:13 +0800

    任务切片

--- Vdom/react.js
+++ Vdom/react.js
@@ -21,10 +21,23 @@ const React = {
   }
 }
 
-const vdom = React.createElement(
-  'div',
-  { id: 'root' },
-  React.createElement('h1', null, 'Hello World'),
-)
+const vdom = React.createElement('div', { id: 'root' }, React.createElement('h1', null, 'Hello World'),)
 
-console.log(vdom)
+// 完成虚拟dom转fiber结构和时间切片
+
+// requestIdleCallback
+
+let nextUnitOfWork = null // 下一个工作单元
+
+function workLoop(deadline) {
+  let shouldYield = false // 是否应该让出时间片
+  while (nextUnitOfWork && !shouldYield) { // 循环执行工作单元
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork) // 执行工作单元
+    shouldYield = deadline.timeRemaining() < 1 // 是否应该让出时间片
+  }
+  requestIdleCallback(workLoop) // 继续执行下一轮循环
+}
+
+requestIdleCallback(workLoop) // 开始执行循环
+
+function performUnitOfWork() { }
