6be00c121c1d57519af1ed8e8e50ac1196118880
Rika-L <2209393334@qq.com>
2024-09-25 11:09:37 +0800

    CSS方案

--- Components/src/components/RiModel/index.tsx
+++ Components/src/components/RiModel/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createPortal } from 'react-dom'
+import styles from './index.module.css'
 
 interface Props {
   hdlClick: () => void
@@ -13,6 +14,8 @@ const RiModel: React.FC<Props> = function ({ hdlClick }) {
           <div className="flex justify-center w-full h-full bg-gray-500/10 fixed z-50 top-0 left-0" onClick={hdlClick}>
             <div className="h-64 w-[512px] bg-white mt-[20vh] rounded-lg" onClick={e => e.stopPropagation()}>
               <div className="font-bold">Dialog Title</div>
+              <div className={styles.a}>hello Rika</div>
+              <div className={styles.b}>hello Rika</div>
             </div>
           </div>,
           document.body,
